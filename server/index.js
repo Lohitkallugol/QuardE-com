7637a14caceaf00c970a02fd9abedb7bd0010e90
Lohit <162778623+Lohitkallugol@users.noreply.github.com>
2024-07-19 20:07:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 // middlewares
 app.use(
   cors({
-    origin: "http://https://rx-Ecommerce.netlify.app",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
